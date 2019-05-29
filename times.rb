03f39ec935ec3684f9176d4eff70791c20562f2e
Kelliann Lafferty <kell10192@gmail.com>
2019-05-29 23:18:49 +0000

    Automatically backed up by Learn

--- times.rb
+++ times.rb
@@ -4,6 +4,23 @@ def using_times
 	end
 end
 
+counter = 0
+while counter < 20
+  puts "The current number is less than 20."
+  counter += 1
+end
+
+num_of_hotdogs_eaten = 0 
+
+while num_of_hotdogs_eaten < 7 
+num_of_hotdogs_eaten += 1 
+puts "You have now eaten #{num_of_hotdogs_eaten} hot dog(s)."
+end
+# => nil (return value)
+
+puts "You ate a total of #{num_of_hotdogs_eaten} hot dogs!"
+# => nil (return value)
+
 
 
 
